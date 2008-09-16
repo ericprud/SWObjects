b48d759c087d179679b069ab9401d2fd48a87d88
Eric Prudhommeaux <eric@w3.org>
2008-09-16 18:57:13 +0000

    ~ isolating POS2BGP structure

--- POS2BGPMap.hpp
+++ POS2BGPMap.hpp
@@ -1,17 +1,21 @@
 /* POS2BGPMap.hpp — association variables with the BGPs in which they appear.
  *
  * POS2BGP does double duty:
- * • For query translation, it determines which variables are coref'd
- *   in which table operations.
  *
- * • For RuleMap query translation, it keeps track of which optionals
- *   in the rule antecedent are needed to produce the data for a given
- *   query, and which correspond to mandatory parts of the query.
+ * • For SQL query translation, POS2BGP determines which variables are coref'd
+ *   in which table operations. This is needed for the mapping from SPARQL
+ *   queries, which have a single namespace for all veriables, to SQL, which
+ *   requires subqueries with different table alias namespaces to express
+ *   certain OPTIONALs and all UNIONs.
  *
- *   For this, WEAK_COREFs record a variable that is weak in all graph
- *   patterns.
+\ * • For RuleMap query translation, POS2BGP keeps track of which OPTIONALs in
+ *   the rule antecedent are needed to produce the data for a given query, and
+ *   which are treated as mandatory because they correspond to mandatory parts
+ *   of the query. Any OPTIONALs which introduce variables referenced in a later
+ *   graph pattern must also be included. <antecedents> maps graph patterns to
+ *   the graph patterns that may affect them.
  *
- * $Id: POS2BGPMap.hpp,v 1.11 2008-09-14 12:13:23 eric Exp $
+ * $Id: POS2BGPMap.hpp,v 1.12 2008-09-16 18:57:13 eric Exp $
  */
 
 #pragma once
@@ -23,9 +27,18 @@
 
 namespace w3c_sw {
 
-    typedef enum {Binding_STRONG = 1, Binding_FILTER = 2, Binding_SELECT = 4, 
+    typedef enum {Binding_NONE = 0x20, 
+		  Binding_STRONG = 1, Binding_FILTER = 2, Binding_SELECT = 4, 
 		  Binding_WEAK = 8, Binding_COREF = 0x10} BindingStrength;
-    typedef std::map<TableOperation*, BindingStrength>	ConsequentMap;
+    class ConsequentMap : public std::map<TableOperation*, BindingStrength> {
+    public:
+	BindingStrength getOperationStrength (TableOperation* bgp) {
+	    ConsequentMap::iterator it = find(bgp);
+	    if (it == end())
+		return Binding_NONE;
+	    return it->second == Binding_STRONG ? Binding_STRONG : Binding_WEAK;
+	}
+    };
     typedef std::map<POS*, ConsequentMap>		ConsequentMapList;
 
     class Consequents {
@@ -329,8 +342,65 @@ namespace w3c_sw {
 	    consequents.clear();
 	}
 
-	ConsequentMapList::iterator begin () { return consequents.begin(); }
-	ConsequentMapList::iterator end () { return consequents.end(); }
+	Variable* _genVar (Variable* base, int index, Result* row, POSFactory* posFactory) {
+	    Variable* ret = NULL;
+	    do {
+		std::stringstream name;
+		name << base->getTerminal() << "_gen" << index;
+		ret = posFactory->getVariable(name.str().c_str());
+		++index;
+	    } while (row->get(ret) != NULL);
+	    return ret;
+	}
+
+	ConsequentMap getIncludeRequiredness (ResultSet* rs, ResultSetIterator row, POSFactory* posFactory) {
+	    ConsequentMap ret;
+	    std::set<Variable*> neededVars;
+
+	    int genNo = 0;
+	    /* 06 — Scan each basic graph pattern GA in the antecedent graph pattern A for variables in triple patterns:
+	     * http://www.w3.org/2008/07/MappingRules/#_06
+	     */
+	    for (ConsequentMapList::iterator maps = consequents.begin();
+		 maps != consequents.end(); ++maps) {
+		Variable* v = dynamic_cast<Variable*>(maps->first);
+		if (v == NULL)
+		    break;
+
+		for (ConsequentMap::iterator bgpBindings = maps->second.begin();
+		     bgpBindings != maps->second.end(); ++bgpBindings) {
+		    /* 06a — If the variable is not weak in S or GA is not optional, tag GA as an included mandatory.
+		     * 06b — If the variable is weak in S and GA is optional and the variable is not in any mandatory antecedent graph, tag GA as an included optional.
+		     * 06c — If the variable is not bound in S, bind it to a novel variable e.g. ?B_gen1
+		     * http://www.w3.org/2008/07/MappingRules/#_06a
+		     * Note that optional variables that are in a mandatory antecedent graph are already removed from consequents.
+		     */
+		    if ((*row)->find(v) == (*row)->end()) {
+			/* The BGP introduces v, which has no correspondence in our query. */
+			neededVars.insert(v);
+			if (bgpBindings->second == Binding_WEAK) {
+			    /* It's optional, so omit it. !!
+			       This is acutally unsound as we
+			       want to include any OPT which
+			       introduces constraints used by
+			       an OPT included later.
+			    */
+#warning not including dependent optionals yet
+			    continue;
+			}
+		    }
+		    ret[bgpBindings->first] = (bgpBindings->second == Binding_STRONG || 
+					       (*row)->get(v) == false) ? Binding_WEAK : Binding_STRONG;
+		}
+		    
+	    }
+
+	    for (std::set<Variable*>::iterator v = neededVars.begin();
+		 v != neededVars.end(); ++v)
+		rs->set(*row, *v, _genVar(*v, genNo++, *row, posFactory), true);
+	    neededVars.clear();
+	    return ret;
+	}
 
 	/* Collect all the POS entries referenced in a particular TableOperation. */
 	std::vector<POS*> entriesFor (TableOperation* op) {
--- RuleInverter.hpp
+++ RuleInverter.hpp
@@ -1,7 +1,7 @@
 /* RuleInverter.hpp - create a SPARQL CONSTRUCT rule that follows 
  * http://www.w3.org/2008/07/MappingRules/#_02
  *
- * $Id: RuleInverter.hpp,v 1.7 2008-09-14 10:50:18 eric Exp $
+ * $Id: RuleInverter.hpp,v 1.8 2008-09-16 18:57:13 eric Exp $
  */
 
 #ifndef RuleInverter_H
@@ -39,14 +39,6 @@ namespace w3c_sw {
 		    (*triple)->construct(p, row);
 	    }
 
-#ifdef NOT_NEEDED
-	    virtual void namedGraphPattern (NamedGraphPattern* self, POS* p_name, bool p_allOpts, ProductionVector<TriplePattern*>* p_TriplePatterns, ProductionVector<Filter*>* p_Filters) {
-		SWObjectDuplicator::namedGraphPattern(self, p_name, p_allOpts, p_TriplePatterns, p_Filters);
-	    }
-	    virtual void defaultGraphPattern (DefaultGraphPattern* self, bool p_allOpts, ProductionVector<TriplePattern*>* p_TriplePatterns, ProductionVector<Filter*>* p_Filters) {
-		SWObjectDuplicator::defaultGraphPattern(self, p_allOpts, p_TriplePatterns, p_Filters);
-	    }
-#endif
 	    /* Overload SWObjectDuplicator::_TableOperations to handle tree depletion. */
 	    virtual void _TableOperations (ProductionVector<TableOperation*>* p_TableOperations, TableJunction* j) {
 		for (std::vector<TableOperation*>::iterator it = p_TableOperations->begin();
@@ -57,33 +49,18 @@ namespace w3c_sw {
 			j->addTableOperation(last.tableOperation);
 		}
 	    }
-#ifdef NOT_NEEDED
-	    virtual void tableDisjunction (TableDisjunction* self, ProductionVector<TableOperation*>* p_TableOperations, ProductionVector<Filter*>* p_Filters) {
-		SWObjectDuplicator::tableDisjunction(self, p_TableOperations, p_Filters);
-	    }
-	    virtual void tableConjunction (TableConjunction* self, ProductionVector<TableOperation*>* p_TableOperations, ProductionVector<Filter*>* p_Filters) {
-		SWObjectDuplicator::tableConjunction(self, p_TableOperations, p_Filters);
-	    }
-#endif
+
 	    virtual void optionalGraphPattern (OptionalGraphPattern* self, TableOperation* p_GroupGraphPattern) {
-		BasicGraphPattern* bgp = dynamic_cast<BasicGraphPattern*>(p_GroupGraphPattern);
 		last.tableOperation = NULL;
-		ConsequentMap::iterator it;
-		if (bgp != NULL && (it = includeRequiredness->find(bgp)) != includeRequiredness->end()) {
-		    if (it->second == Binding_STRONG) {
+		BindingStrength s = includeRequiredness->getOperationStrength(p_GroupGraphPattern);
+		if (s != Binding_NONE) {
+		    if (s == Binding_STRONG)
 			// let p_GroupGraphPattern set last.tableOperation
 			p_GroupGraphPattern->express(this);
-		    } else {
-			std::cerr << "OPTIONAL: " << includeRequiredness->find(bgp)->second << std::endl;
+		    else
 			SWObjectDuplicator::optionalGraphPattern(self, p_GroupGraphPattern);
-		    }
 		}
 	    }
-#ifdef NOT_NEEDED
-	    virtual void graphGraphPattern (GraphGraphPattern* self, POS* p_POS, TableOperation* p_GroupGraphPattern) {
-		SWObjectDuplicator::graphGraphPattern(self, p_POS, p_GroupGraphPattern);
-	    }
-#endif
 	};
 
 
@@ -101,16 +78,6 @@ namespace w3c_sw {
 	~MappingConstruct () {
 	    delete m_MappedAntecedent;
 	}
-	Variable* _genVar (Variable* base, int index, Result* row) {
-	    Variable* ret = NULL;
-	    do {
-		std::stringstream name;
-		name << base->getTerminal() << "_gen" << index;
-		ret = posFactory->getVariable(name.str().c_str());
-		++index;
-	    } while (row->get(ret) != NULL);
-	    return ret;
-	}
 	virtual OperationResultSet* execute (RdfDB* db, ResultSet* rs = NULL) {
 	    OperationResultSet* opRS = dynamic_cast<OperationResultSet*>(rs);
 	    if (opRS == NULL)
@@ -127,52 +94,11 @@ namespace w3c_sw {
 
 	    for (ResultSetIterator row = opRS->begin() ; row != opRS->end(); ++row) {
 
-		/* Set of { BGP => optional } associations to indicate if the BGP is optional. */
-		ConsequentMap includeRequiredness;
-		std::set<Variable*> neededVars;
-
-		int genNo = 0;
 		/* 06 — Scan each basic graph pattern GA in the antecedent graph pattern A for variables in triple patterns:
 		 * http://www.w3.org/2008/07/MappingRules/#_06
 		 */
-		for (ConsequentMapList::iterator maps = consequents.begin();
-		     maps != consequents.end(); ++maps) {
-		    Variable* v = dynamic_cast<Variable*>(maps->first);
-		    if (v == NULL)
-			break;
-
-		    for (ConsequentMap::iterator bgpBindings = maps->second.begin();
-			 bgpBindings != maps->second.end(); ++bgpBindings) {
-			/* 06a — If the variable is not weak in S or GA is not optional, tag GA as an included mandatory.
-			 * 06b — If the variable is weak in S and GA is optional and the variable is not in any mandatory antecedent graph, tag GA as an included optional.
-			 * 06c — If the variable is not bound in S, bind it to a novel variable e.g. ?B_gen1
-			 * http://www.w3.org/2008/07/MappingRules/#_06a
-			 * Note that optional variables that are in a mandatory antecedent graph are already removed from consequents.
-			 */
-			if ((*row)->find(v) == (*row)->end()) {
-			    /* The BGP introduces v, which has no correspondence in our query. */
-			    neededVars.insert(v);
-			    if (bgpBindings->second == Binding_WEAK) {
-				/* It's optional, so omit it. !!
-				   This is acutally unsound as we may
-				   want to include any OPT which
-				   introduces constraints used by a
-				   later included OPT.
-				 */
-#warning not including dependent optionals yet
-				continue;
-			    }
-			}
-			includeRequiredness[bgpBindings->first] = (bgpBindings->second == Binding_STRONG || 
-								   (*row)->get(v) == false) ? Binding_WEAK : Binding_STRONG;
-		    }
-		    
-		}
-
-		for (std::set<Variable*>::iterator v = neededVars.begin();
-		     v != neededVars.end(); ++v)
-		    opRS->set(*row, *v, _genVar(*v, genNo++, *row), true);
-		neededVars.clear();
+		/* Set of { BGP => optional } associations to indicate if the BGP is optional. */
+		ConsequentMap includeRequiredness = consequents.getIncludeRequiredness(opRS, row, posFactory);
 
 		/* 07 — Substitute the values in S for the variables in A.
 		 * 08 — Create a stem query disjoint DQS which is a reproduction of the mapping rule antecedent A
