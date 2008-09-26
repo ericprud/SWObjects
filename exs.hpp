7e6f2b30e08ffea1d218f597ddb020e3c860b8d6
Eric Prudhommeaux <eric@w3.org>
2008-09-26 18:23:56 +0000

    + var args (vprintf) style FAIL messages

--- exs.hpp
+++ exs.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <cstdarg>
 #include "SWObjects.hpp"
 
 using namespace std;
@@ -11,11 +12,13 @@ using namespace std;
 vector<string> journal;
 
 struct barf : w3c_sw::StringException {
-    barf (char const* type, char const* file, size_t line, const char* str) : 
-	w3c_sw::StringException(make(type, file, line, str)) {  }
+    barf (char const* type, char const* file, size_t line, const char* fmt, va_list args) : 
+	w3c_sw::StringException(make(type, file, line, fmt, args)) {  }
 protected:
-    std::string make (char const* type, string file, size_t line, char const* str) {
+    std::string make (char const* type, string file, size_t line, char const* fmt, va_list args) {
+	static char str[1024];
 	stringstream s;
+	vsprintf(str, fmt, args);
 	s << type << ": " << endl << file << ":" << line << ": " << str << endl;
 	for (vector<string>::reverse_iterator it = journal.rbegin();
 	     it != journal.rend(); ++it)
@@ -41,8 +44,18 @@ public:
     }
 };
 
+barf varBarfer (char const* type, char const* file, size_t line, char const* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    barf ret = barf(type, file,  line, fmt, args);
+    va_end(args);
+    return ret;
+}
+
 #define MARK JournalState _(__FILE__,  __LINE__, __PRETTY_FUNCTION__)
 #define START(X) JournalState _(__FILE__,  __LINE__, X)
 #define NOW(X) JournalState::now(__FILE__,  __LINE__, X)
-#define FAIL(X) throw(barf("FAIL", __FILE__,  __LINE__, X))
+#define FAIL(X) throw(varBarfer("FAIL", __FILE__,  __LINE__, X))
+#define FAIL1(X, a) throw(varBarfer("FAIL", __FILE__,  __LINE__, X, a))
+#define FAIL2(X, a, b) throw(varBarfer("FAIL", __FILE__,  __LINE__, X, a, b))
 
