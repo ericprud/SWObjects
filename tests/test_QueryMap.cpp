6eca3032a92da6f1e88fb4ac341fb682c31cdc7d
Eric Prudhommeaux <eric@w3.org>
2009-02-13 22:10:16 +0000

    + start on map tests

---
+++ tests/test_QueryMap.cpp
@@ -0,0 +1,77 @@
+/* test query-mapping.
+ *
+ * $Id: test_QueryMap.cpp,v 1.5 2008-12-04 22:37:09 eric Exp $
+ */
+
+#define BOOST_TEST_DYN_LINK 1
+#define BOOST_TEST_MODULE QueryMap
+#include <boost/test/unit_test.hpp>
+//#include <boost/test/included/unit_test.hpp>
+
+#include <map>
+#include <vector>
+#include "SWObjects.hpp"
+#include "SPARQLfedParser.hpp"
+#include "RdfDB.hpp"
+#include "ResultSet.hpp"
+#include "QueryMapper.hpp"
+#include "SQLizer.hpp"
+
+
+using namespace w3c_sw;
+
+POSFactory f;
+SPARQLfedDriver sparqlParser("", &f);
+std::ostream* DebugStream = NULL;
+QueryMapper queryMapper(&f, &DebugStream);
+
+
+void mapTest (const char* queryFile, const char* mapFile, 
+	      const char* mapResultsFile, 
+	      const char* stemURI, const char* sqlResultsFile) {
+
+    /* Parse query. */
+    if (sparqlParser.parse_file(queryFile)) {
+	std::string msg = std::string("failed to parse query \"") + 
+	    queryFile + std::string("\".");
+	throw msg;
+    }
+    Operation* query = sparqlParser.root;
+
+    /* Parse map. */
+    if (sparqlParser.parse_file(mapFile)) {
+	std::string msg = std::string("failed to parse map \"") + 
+	    mapFile + std::string("\".");
+	throw msg;
+    }
+    queryMapper.addRule(dynamic_cast<Construct*>(sparqlParser.root));
+    delete sparqlParser.root;
+
+    query->express(&queryMapper);
+    const Operation* o = queryMapper.getCopy();
+    delete query;
+
+    /* Parse map results. */
+    if (sparqlParser.parse_file(mapResultsFile)) {
+	std::string msg = std::string("failed to parse map results \"") + 
+	    mapResultsFile + std::string("\".");
+	throw msg;
+    }
+    Operation* mapResults = sparqlParser.root;
+    BOOST_CHECK_EQUAL(*o, *mapResults);
+    delete mapResults;
+
+    if (stemURI != NULL) {
+	char predicateDelims[]={'#',' ',' '};
+	char nodeDelims[]={'/','.',' '};
+	SQLizer s(stemURI, predicateDelims, nodeDelims, "id", NULL);
+	o->express(&s);
+	std::cout << s.getSQLstring() << endl;
+    }
+    delete o;
+}
+
+BOOST_AUTO_TEST_CASE( map_1 ) {
+    mapTest("bsbm/q1.rq", "bsbm/ruleMap.rq", "bsbm/q1-db.rq", NULL, NULL);
+}
+
