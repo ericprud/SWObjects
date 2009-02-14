1969f2c7ac6d2ad420ecd4e8527dc2c309dc2518
Eric Prudhommeaux <eric@w3.org>
2009-02-14 04:58:07 +0000

    + operator== tests

--- tests/test_QueryMap.cpp
+++ tests/test_QueryMap.cpp
@@ -26,6 +26,33 @@ std::ostream* DebugStream = NULL;
 QueryMapper queryMapper(&f, &DebugStream);
 
 
+void equalsTest (const char* leftString, const char* rightString, bool expect) {
+
+    /* Parse left. */
+    if (sparqlParser.parse_string(leftString)) {
+	std::string msg = std::string("failed to parse left \"") + 
+	    leftString + std::string("\".");
+	throw msg;
+    }
+    Operation* left = sparqlParser.root;
+
+    /* Parse right. */
+    if (sparqlParser.parse_string(rightString)) {
+	std::string msg = std::string("failed to parse right \"") + 
+	    rightString + std::string("\".");
+	throw msg;
+    }
+    Operation* right = sparqlParser.root;
+
+    if (expect == true)
+	BOOST_CHECK_EQUAL(*left, *right);
+    else
+	BOOST_CHECK_MESSAGE( !(*left == *right), *left << " == " << *right );
+
+    delete left;
+    delete right;
+}
+
 void mapTest (const char* queryFile, const char* mapFile, 
 	      const char* mapResultsFile, 
 	      const char* stemURI, const char* sqlResultsFile) {
@@ -71,6 +98,20 @@ void mapTest (const char* queryFile, const char* mapFile,
     delete o;
 }
 
+BOOST_AUTO_TEST_CASE( op_equals ) {
+    equalsTest("SELECT * WHERE { ?s ?p ?o }", 
+	       "select*{?s?p?o}", true);
+    equalsTest("SELECT ?s ?p ?o { ?s ?p ?o }", 
+	       "SELECT ?s ?p $o { ?s ?p ?o}", true);
+    equalsTest("SELECT ?p ?s ?o { ?s ?p ?o }", 
+	       "SELECT ?s ?p ?o { ?s ?p ?o}", true);
+    equalsTest("SELECT ?s ?p ?o { ?s ?p ?o }", 
+	       "SELECT ?s ?p $o { ?s ?o ?p}", false);
+    equalsTest("SELECT * { FILTER(1*(2+3)=?x) }", 
+	       "SELECT * { FILTER(1*(2+3)=$x) }", true);
+    equalsTest("SELECT * { FILTER( ?x ) }", 
+	       "SELECT * { FILTER( ?y ) }", false);
+}
 BOOST_AUTO_TEST_CASE( map_1 ) {
     mapTest("bsbm/q1.rq", "bsbm/ruleMap.rq", "bsbm/q1-db.rq", NULL, NULL);
 }
