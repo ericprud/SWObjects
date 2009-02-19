466789580516df075bfe55344046e90a26ae3855
Eric Prudhommeaux <eric@w3.org>
2009-02-19 21:03:41 +0000

    + coi (healthCare) simple tests

--- tests/test_QueryMap.cpp
+++ tests/test_QueryMap.cpp
@@ -22,7 +22,7 @@ using namespace w3c_sw;
 
 POSFactory f;
 SPARQLfedDriver sparqlParser("", &f);
-std::ostream* DebugStream = NULL; // e.g. &std::cerr
+std::ostream* DebugStream = NULL; // &std::cerr;
 QueryMapper queryMapper(&f, &DebugStream);
 
 
@@ -53,50 +53,55 @@ void equalsTest (const char* leftString, const char* rightString, bool expect) {
     delete right;
 }
 
-void mapTest (const char* queryFile, const char* mapFile, 
-	      const char* mapResultsFile, 
-	      const char* stemURI, const char* sqlResultsFile) {
+struct MapTest {
+    const Operation* transformed;
+    Operation* mapResults;
+    MapTest (const char* queryFile, const char* mapFile, 
+	     const char* mapResultsFile, 
+	     const char* stemURI, const char* sqlResultsFile) {
 
-    /* Parse query. */
-    if (sparqlParser.parse_file(queryFile)) {
-	std::string msg = std::string("failed to parse query \"") + 
-	    queryFile + std::string("\".");
-	throw msg;
+	/* Parse query. */
+	if (sparqlParser.parse_file(queryFile)) {
+	    std::string msg = std::string("failed to parse query \"") + 
+		queryFile + std::string("\".");
+	    throw msg;
+	}
+	Operation* query = sparqlParser.root;
+
+	/* Parse map. */
+	if (sparqlParser.parse_file(mapFile)) {
+	    std::string msg = std::string("failed to parse map \"") + 
+		mapFile + std::string("\".");
+	    throw msg;
+	}
+	queryMapper.addRule(dynamic_cast<Construct*>(sparqlParser.root));
+	delete sparqlParser.root;
+
+	query->express(&queryMapper);
+	transformed = queryMapper.last.operation;
+	delete query;
+
+	/* Parse map results. */
+	if (sparqlParser.parse_file(mapResultsFile)) {
+	    std::string msg = std::string("failed to parse map results \"") + 
+		mapResultsFile + std::string("\".");
+	    throw msg;
+	}
+	mapResults = sparqlParser.root;
+
+	if (stemURI != NULL) {
+	    char predicateDelims[]={'#',' ',' '};
+	    char nodeDelims[]={'/','.',' '};
+	    SQLizer s(stemURI, predicateDelims, nodeDelims, "id", &DebugStream);
+	    transformed->express(&s);
+	    std::cout << s.getSQLstring() << endl;
+	}
     }
-    Operation* query = sparqlParser.root;
-
-    /* Parse map. */
-    if (sparqlParser.parse_file(mapFile)) {
-	std::string msg = std::string("failed to parse map \"") + 
-	    mapFile + std::string("\".");
-	throw msg;
+    ~MapTest () {
+	delete mapResults;
+	delete transformed;
     }
-    queryMapper.addRule(dynamic_cast<Construct*>(sparqlParser.root));
-    delete sparqlParser.root;
-
-    query->express(&queryMapper);
-    const Operation* transformed = queryMapper.last.operation;
-    delete query;
-
-    /* Parse map results. */
-    if (sparqlParser.parse_file(mapResultsFile)) {
-	std::string msg = std::string("failed to parse map results \"") + 
-	    mapResultsFile + std::string("\".");
-	throw msg;
-    }
-    Operation* mapResults = sparqlParser.root;
-    BOOST_CHECK_EQUAL(*transformed, *mapResults);
-    delete mapResults;
-
-    if (stemURI != NULL) {
-	char predicateDelims[]={'#',' ',' '};
-	char nodeDelims[]={'/','.',' '};
-	SQLizer s(stemURI, predicateDelims, nodeDelims, "id", NULL);
-	transformed->express(&s);
-	std::cout << s.getSQLstring() << endl;
-    }
-    delete transformed;
-}
+};
 
 BOOST_AUTO_TEST_CASE( op_equals ) {
     equalsTest("SELECT * WHERE { ?s ?p ?o }", 
@@ -112,7 +117,17 @@ BOOST_AUTO_TEST_CASE( op_equals ) {
     equalsTest("SELECT * { FILTER( ?x ) }", 
 	       "SELECT * { FILTER( ?y ) }", false);
 }
-BOOST_AUTO_TEST_CASE( map_1 ) {
-    mapTest("bsbm/q1.rq", "bsbm/ruleMap.rq", "bsbm/q1-db.rq", NULL, NULL);
+
+BOOST_AUTO_TEST_CASE( simple_hl7_sdtm ) {
+    MapTest t("tests/healthCare/simple/sdtm.rq", "tests/healthCare/simple/hl7-sdtm.rq", "tests/healthCare/simple/hl7.rq", NULL, NULL);
+    BOOST_CHECK_EQUAL(*t.transformed, *t.mapResults);
+}
+BOOST_AUTO_TEST_CASE( simple_sdtm_db ) {
+    MapTest t("tests/healthCare/simple/hl7.rq", "tests/healthCare/simple/db-hl7.rq", "tests/healthCare/simple/db.rq", "http://hospital.example/DB/", "tests/healthCare/simple/db.sql");
+    BOOST_CHECK_EQUAL(*t.transformed, *t.mapResults);
+}
+BOOST_AUTO_TEST_CASE( bsbm_1 ) {
+    MapTest t("bsbm/q1.rq", "bsbm/ruleMap.rq", "bsbm/q1-db.rq", NULL, NULL);
+    BOOST_CHECK_EQUAL(*t.transformed, *t.mapResults);
 }
 
