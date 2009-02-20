2ae6f8e05bf05f812d39ab5c7adfd2a49d70f18c
Eric Prudhommeaux <eric@w3.org>
2009-02-20 04:44:05 +0000

    + healthCare tests (from HCLSIG COI task force)

--- tests/test_QueryMap.cpp
+++ tests/test_QueryMap.cpp
@@ -26,39 +26,145 @@ std::ostream* DebugStream = NULL; // &std::cerr;
 QueryMapper queryMapper(&f, &DebugStream);
 
 
-void equalsTest (const char* leftString, const char* rightString, bool expect) {
+struct EqualsTest {
+    Operation* left, * right;
+    EqualsTest (const char* leftString, const char* rightString) {
 
-    /* Parse left. */
-    if (sparqlParser.parse_string(leftString)) {
-	std::string msg = std::string("failed to parse left \"") + 
-	    leftString + std::string("\".");
-	throw msg;
+	/* Parse left. */
+	if (sparqlParser.parse_string(leftString)) {
+	    std::string msg = std::string("failed to parse left \"") + 
+		leftString + std::string("\".");
+	    throw msg;
+	}
+	left = sparqlParser.root;
+
+	/* Parse right. */
+	if (sparqlParser.parse_string(rightString)) {
+	    std::string msg = std::string("failed to parse right \"") + 
+		rightString + std::string("\".");
+	    throw msg;
+	}
+	right = sparqlParser.root;
     }
-    Operation* left = sparqlParser.root;
-
-    /* Parse right. */
-    if (sparqlParser.parse_string(rightString)) {
-	std::string msg = std::string("failed to parse right \"") + 
-	    rightString + std::string("\".");
-	throw msg;
+    ~EqualsTest () {
+	delete left;
+	delete right;
     }
-    Operation* right = sparqlParser.root;
+};
 
-    if (expect == true)
-	BOOST_CHECK_EQUAL(*left, *right);
-    else
-	BOOST_CHECK_MESSAGE( !(*left == *right), *left << " == " << *right );
-
-    delete left;
-    delete right;
+BOOST_AUTO_TEST_SUITE( op_equals )
+BOOST_AUTO_TEST_SUITE( bgp )
+BOOST_AUTO_TEST_CASE( case_and_space_sensitivity ) {
+    BOOST_TEST_CHECKPOINT( "executing BGP tests" );
+    EqualsTest t("SELECT * WHERE { ?s ?p ?o }", 
+		 "select*{?s?p?o}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
 }
+BOOST_AUTO_TEST_CASE( question_dollar_vars ) {
+    EqualsTest t("SELECT ?s ?p ?o { ?s ?p ?o }", 
+		 "SELECT ?s ?p $o { ?s ?p ?o}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( ignore_select_order ) {
+    EqualsTest t("SELECT ?p ?s ?o { ?s ?p ?o }", 
+		 "SELECT ?s ?p ?o { ?s ?p ?o}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( observe_triple_order ) {
+    EqualsTest t("SELECT ?s ?p ?o { ?s ?p ?o }", 
+		 "SELECT ?s ?p $o { ?s ?o ?p}");
+    BOOST_CHECK_MESSAGE( !(*t.left == *t.right), *t.left << " == " << *t.right );
+}
+BOOST_AUTO_TEST_CASE( equiv_filter ) {
+    EqualsTest t("SELECT * { FILTER(1*(2+3)=?x) }", 
+		 "SELECT * { FILTER(1*(2+3)=$x) }");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( different_vars_in_filter ) {
+    EqualsTest t("SELECT * { FILTER( ?x ) }", 
+		 "SELECT * { FILTER( ?y ) }");
+    BOOST_CHECK_MESSAGE( !(*t.left == *t.right), *t.left << " == " << *t.right );
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE( nested )
+BOOST_AUTO_TEST_CASE( nested_case_and_space_sensitivity ) {
+    BOOST_TEST_CHECKPOINT( "executing nested tests" );
+    EqualsTest t("SELECT * WHERE {{ ?s ?p ?o }}", 
+		 "select*{{?s?p?o}}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_question_dollar_vars ) {
+    EqualsTest t("SELECT ?s ?p ?o {{ ?s ?p ?o }}", 
+		 "SELECT ?s ?p $o {{ ?s ?p ?o}}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_ignore_select_order ) {
+    EqualsTest t("SELECT ?p ?s ?o {{ ?s ?p ?o }}", 
+		 "SELECT ?s ?p ?o {{ ?s ?p ?o}}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_observe_triple_order ) {
+    EqualsTest t("SELECT ?s ?p ?o {{ ?s ?p ?o }}", 
+		 "SELECT ?s ?p $o {{ ?s ?o ?p}}");
+    BOOST_CHECK_MESSAGE( !(*t.left == *t.right), *t.left << " == " << *t.right );
+}
+BOOST_AUTO_TEST_CASE( nested_equiv_filter ) {
+    EqualsTest t("SELECT * {{ FILTER(1*(2+3)=?x) }}", 
+		 "SELECT * {{ FILTER(1*(2+3)=$x) }}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_different_vars_in_filter ) {
+    EqualsTest t("SELECT * {{ FILTER( ?x ) }}", 
+		 "SELECT * {{ FILTER( ?y ) }}");
+    BOOST_CHECK_MESSAGE( !(*t.left == *t.right), *t.left << " == " << *t.right );
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE( named )
+BOOST_AUTO_TEST_CASE( nested_case_and_space_sensitivity ) {
+    BOOST_TEST_CHECKPOINT( "executing named graph tests" );
+    EqualsTest t("SELECT * WHERE {GRAPH<x>{ ?s ?p ?o }}", 
+		 "select*{GRAPH<x>{?s?p?o}}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_question_dollar_vars ) {
+    EqualsTest t("SELECT ?s ?p ?o {GRAPH<x>{ ?s ?p ?o }}", 
+		 "SELECT ?s ?p $o {GRAPH<x>{ ?s ?p ?o}}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_ignore_select_order ) {
+    EqualsTest t("SELECT ?p ?s ?o {GRAPH<x>{ ?s ?p ?o }}", 
+		 "SELECT ?s ?p ?o {GRAPH<x>{ ?s ?p ?o}}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_observe_triple_order ) {
+    EqualsTest t("SELECT ?s ?p ?o {GRAPH<x>{ ?s ?p ?o }}", 
+		 "SELECT ?s ?p $o {GRAPH<x>{ ?s ?o ?p}}");
+    BOOST_CHECK_MESSAGE( !(*t.left == *t.right), *t.left << " == " << *t.right );
+}
+BOOST_AUTO_TEST_CASE( nested_equiv_filter ) {
+    EqualsTest t("SELECT * {GRAPH<x>{ FILTER(1*(2+3)=?x) }}", 
+		 "SELECT * {GRAPH<x>{ FILTER(1*(2+3)=$x) }}");
+    BOOST_CHECK_EQUAL(*t.left, *t.right);
+}
+BOOST_AUTO_TEST_CASE( nested_different_vars_in_filter ) {
+    EqualsTest t("SELECT * {GRAPH<x>{ FILTER( ?x ) }}", 
+		 "SELECT * {GRAPH<x>{ FILTER( ?y ) }}");
+    BOOST_CHECK_MESSAGE( !(*t.left == *t.right), *t.left << " == " << *t.right );
+}
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+
 
 struct MapTest {
+    bool bgpCompareVars;
     const Operation* transformed;
     Operation* mapResults;
     MapTest (const char* queryFile, const char* mapFile, 
 	     const char* mapResultsFile, 
-	     const char* stemURI, const char* sqlResultsFile) {
+	     const char* stemURI, const char* sqlResultsFile) :
+	bgpCompareVars(BasicGraphPattern::CompareVars) {
 
 	/* Parse query. */
 	if (sparqlParser.parse_file(queryFile)) {
@@ -96,36 +202,28 @@ struct MapTest {
 	    transformed->express(&s);
 	    std::cout << s.getSQLstring() << endl;
 	}
+	BasicGraphPattern::CompareVars = true;
     }
     ~MapTest () {
+	BasicGraphPattern::CompareVars = bgpCompareVars;
 	delete mapResults;
 	delete transformed;
     }
 };
 
-BOOST_AUTO_TEST_CASE( op_equals ) {
-    equalsTest("SELECT * WHERE { ?s ?p ?o }", 
-	       "select*{?s?p?o}", true);
-    equalsTest("SELECT ?s ?p ?o { ?s ?p ?o }", 
-	       "SELECT ?s ?p $o { ?s ?p ?o}", true);
-    equalsTest("SELECT ?p ?s ?o { ?s ?p ?o }", 
-	       "SELECT ?s ?p ?o { ?s ?p ?o}", true);
-    equalsTest("SELECT ?s ?p ?o { ?s ?p ?o }", 
-	       "SELECT ?s ?p $o { ?s ?o ?p}", false);
-    equalsTest("SELECT * { FILTER(1*(2+3)=?x) }", 
-	       "SELECT * { FILTER(1*(2+3)=$x) }", true);
-    equalsTest("SELECT * { FILTER( ?x ) }", 
-	       "SELECT * { FILTER( ?y ) }", false);
-}
-
-BOOST_AUTO_TEST_CASE( simple_hl7_sdtm ) {
+BOOST_AUTO_TEST_SUITE( healthCare )
+BOOST_AUTO_TEST_SUITE( simple )
+BOOST_AUTO_TEST_CASE( hl7_sdtm ) {
     MapTest t("tests/healthCare/simple/sdtm.rq", "tests/healthCare/simple/hl7-sdtm.rq", "tests/healthCare/simple/hl7.rq", NULL, NULL);
     BOOST_CHECK_EQUAL(*t.transformed, *t.mapResults);
 }
-BOOST_AUTO_TEST_CASE( simple_sdtm_db ) {
+BOOST_AUTO_TEST_CASE( sdtm_db ) {
     MapTest t("tests/healthCare/simple/hl7.rq", "tests/healthCare/simple/db-hl7.rq", "tests/healthCare/simple/db.rq", "http://hospital.example/DB/", "tests/healthCare/simple/db.sql");
     BOOST_CHECK_EQUAL(*t.transformed, *t.mapResults);
 }
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_CASE( bsbm_1 ) {
     MapTest t("bsbm/q1.rq", "bsbm/ruleMap.rq", "bsbm/q1-db.rq", NULL, NULL);
     BOOST_CHECK_EQUAL(*t.transformed, *t.mapResults);
