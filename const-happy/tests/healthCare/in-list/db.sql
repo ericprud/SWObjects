4e5ce3e74685845944a7c46a26ecb79b3ceffab2
Eric Prudhommeaux <eric@w3.org>
2009-03-01 17:56:26 +0000

    undo mis-move

--- const-happy/tests/healthCare/in-list/db.sql
+++
@@ -1,9 +0,0 @@
-SELECT patient.id AS patient, patient.DateOfBirth AS dob, sexEntry_gen1.EntryName AS sex, indicItem_gen2.EntryName AS takes, indicItem_gen2.PerformedDTTM AS indicDate
-       FROM Person AS patient
-            INNER JOIN Sex_DE AS sexEntry_gen1 ON sexEntry_gen1.id=patient.SexDE
-            INNER JOIN Item_Medication AS indicItem_gen2 ON indicItem_gen2.PatientID=patient.id
-            INNER JOIN Medication AS indicMed_gen5 ON indicMed_gen5.ItemID=indicItem_gen2.id
-            INNER JOIN Medication_DE AS indicDE_gen4 ON indicDE_gen4.id=indicMed_gen5.MedDictDE
-            INNER JOIN NDCcodes AS indicCode_gen0 ON indicCode_gen0.NDC=indicDE_gen4.NDC
- WHERE (indicCode_gen0.ingredient = 6809 OR indicCode_gen0.ingredient = 6810)
- LIMIT 30
--- const-happy/tests/healthCare/simple/db.sql
+++
@@ -1,8 +0,0 @@
-SELECT patient.id AS patient, patient.DateOfBirth AS dob, sexEntry_gen2.EntryName AS sex, indicItem_gen4.EntryName AS takes, indicItem_gen4.PerformedDTTM AS indicDate
-       FROM Person AS patient
-            INNER JOIN Sex_DE AS sexEntry_gen2 ON sexEntry_gen2.id=patient.SexDE
-            INNER JOIN Item_Medication AS indicItem_gen4 ON indicItem_gen4.PatientID=patient.id
-            INNER JOIN Medication AS indicMed_gen0 ON indicMed_gen0.ItemID=indicItem_gen4.id
-            INNER JOIN Medication_DE AS indicDE_gen5 ON indicDE_gen5.id=indicMed_gen0.MedDictDE
-            INNER JOIN NDCcodes AS indicCode_gen3 ON indicCode_gen3.NDC=indicDE_gen5.NDC AND indicCode_gen3.ingredient=6809
- LIMIT 30
