3e559454c36801f1563c74ebca3bbce7cd337458
Eric Prudhommeaux <eric@w3.org>
2009-03-08 21:12:00 +0000

    ~ reworked test to be able to handle test strings as well as file refs

--- tests/test_DAWG.cpp
+++ tests/test_DAWG.cpp
@@ -12,6 +12,8 @@
 //#include <boost/test/included/unit_test.hpp>
 
 #include <fstream>
+#include <sstream>
+#include <iostream>
 #include <iterator>
 #include "SWObjects.hpp"
 #include "SPARQLfedParser/SPARQLfedParser.hpp"
@@ -45,20 +47,20 @@ struct TestResultSet : public ResultSet {
 	turtleParser.setGraph(d.assureGraph(graphName));
 	turtleParser.parse_file(fileName);
     }
-    TestResultSet (const char* defGraphs[], const char* namGraphs[], 
-		   const char* queryFile) : ResultSet(&F) {
+    TestResultSet (std::istream defGraphs[], size_t defCount, 
+		   const char* namGraphs[], 
+		   std::istream& query) : ResultSet(&F) {
  
 	/* Parse query. */
-	if (sparqlParser.parse_file(queryFile)) {
-	    std::string msg = std::string("failed to parse query \"") + 
-		queryFile + std::string("\".");
-	    throw msg;
-	}
+	if (sparqlParser.parse_stream(query))
+	    throw std::string("failed to parse query.");
 
 	/* Parse data. */
 	if (defGraphs != NULL)
-	    for (size_t i = 0; defGraphs[i] != Sentinel; ++i)
-		_loadGraphWrapper(NULL, defGraphs[i]);
+	    for (size_t i = 0; i != defCount; ++i) {
+		turtleParser.setGraph(d.assureGraph(NULL));
+		turtleParser.parse_stream(defGraphs[i]);
+	    }
 
 	if (namGraphs != NULL)
 	    for (size_t i = 0; namGraphs[i] != Sentinel; ++i)
@@ -78,123 +80,141 @@ std::ostream& operator<< (std::ostream& os, TestResultSet const& my) {
 }
 
 /* Macros for terse test syntax: */
-#define DEFGRAPH_TEST(QUERY_FILE, RESULT_FILE) \
-    TestResultSet measured(defaultGraphs, NULL, QUERY_FILE); \
+#define DEFGRAPH_FILE_TEST(QUERY_FILE, RESULT_FILE) \
+    std::ifstream query(QUERY_FILE); \
+    if (!query.is_open()) throw std::string("failed to open query file ") + QUERY_FILE; \
+    TestResultSet measured(defaultGraphs, sizeof(defaultGraphs)/sizeof(defaultGraphs[0]), NULL, query); \
     ResultSet expected(&F, &P, RESULT_FILE); \
     BOOST_CHECK_EQUAL(measured, expected);
 #define S Sentinel
 
 BOOST_AUTO_TEST_SUITE( basic )
+
 BOOST_AUTO_TEST_CASE( Basic_graph_pattern___spoo ) {
-    const char* defaultGraphs[] ={ "data-r2/basic/data-6.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/spoo-1.rq", "data-r2/basic/spoo-1.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-6.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/spoo-1.rq", "data-r2/basic/spoo-1.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___List_1 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-2.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/list-1.rq", "data-r2/basic/list-1.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-2.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/list-1.rq", "data-r2/basic/list-1.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___List_2 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-2.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/list-2.rq", "data-r2/basic/list-2.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-2.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/list-2.rq", "data-r2/basic/list-2.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___List_3 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-2.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/list-3.rq", "data-r2/basic/list-3.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-2.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/list-3.rq", "data-r2/basic/list-3.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___List_4 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-2.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/list-4.rq", "data-r2/basic/list-4.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-2.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/list-4.rq", "data-r2/basic/list-4.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Prefix_Base_1 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-1.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/base-prefix-1.rq", "data-r2/basic/base-prefix-1.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-1.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/base-prefix-1.rq", "data-r2/basic/base-prefix-1.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Prefix_Base_2 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-1.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/base-prefix-2.rq", "data-r2/basic/base-prefix-2.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-1.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/base-prefix-2.rq", "data-r2/basic/base-prefix-2.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Prefix_Base_3 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-1.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/base-prefix-3.rq", "data-r2/basic/base-prefix-3.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-1.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/base-prefix-3.rq", "data-r2/basic/base-prefix-3.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Prefix_Base_4 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-1.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/base-prefix-4.rq", "data-r2/basic/base-prefix-4.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-1.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/base-prefix-4.rq", "data-r2/basic/base-prefix-4.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Prefix_Base_5 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-1.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/base-prefix-5.rq", "data-r2/basic/base-prefix-5.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-1.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/base-prefix-5.rq", "data-r2/basic/base-prefix-5.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Quotes_1 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-3.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/quotes-1.rq", "data-r2/basic/quotes-1.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-3.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/quotes-1.rq", "data-r2/basic/quotes-1.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Quotes_2 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-3.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/quotes-2.rq", "data-r2/basic/quotes-2.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-3.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/quotes-2.rq", "data-r2/basic/quotes-2.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Quotes_3 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-3.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/quotes-3.rq", "data-r2/basic/quotes-3.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-3.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/quotes-3.rq", "data-r2/basic/quotes-3.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Quotes_4 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-3.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/quotes-4.rq", "data-r2/basic/quotes-4.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-3.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/quotes-4.rq", "data-r2/basic/quotes-4.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_1 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-1.rq", "data-r2/basic/term-1.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-1.rq", "data-r2/basic/term-1.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_2 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-2.rq", "data-r2/basic/term-2.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-2.rq", "data-r2/basic/term-2.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_3 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-3.rq", "data-r2/basic/term-3.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-3.rq", "data-r2/basic/term-3.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_4 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-4.rq", "data-r2/basic/term-4.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-4.rq", "data-r2/basic/term-4.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_5 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-5.rq", "data-r2/basic/term-5.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-5.rq", "data-r2/basic/term-5.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_6 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-6.rq", "data-r2/basic/term-6.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-6.rq", "data-r2/basic/term-6.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_7 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-7.rq", "data-r2/basic/term-7.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-7.rq", "data-r2/basic/term-7.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_8 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-8.rq", "data-r2/basic/term-8.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-8.rq", "data-r2/basic/term-8.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Term_9 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-4.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/term-9.rq", "data-r2/basic/term-9.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-4.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/term-9.rq", "data-r2/basic/term-9.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Var_1 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-5.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/var-1.rq", "data-r2/basic/var-1.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-5.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/var-1.rq", "data-r2/basic/var-1.srx");
 }
 BOOST_AUTO_TEST_CASE( Basic___Var_2 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-5.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/var-2.rq", "data-r2/basic/var-2.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-5.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/var-2.rq", "data-r2/basic/var-2.srx");
 }
 BOOST_AUTO_TEST_CASE( Non_matching_triple_pattern ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-7.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/bgp-no-match.rq", "data-r2/basic/bgp-no-match.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-7.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/bgp-no-match.rq", "data-r2/basic/bgp-no-match.srx");
 }
 BOOST_AUTO_TEST_CASE( Prefix_name_1 ) {
-    const char* defaultGraphs[] = { "data-r2/basic/data-6.ttl", S };
-    DEFGRAPH_TEST("data-r2/basic/prefix-name-1.rq", "data-r2/basic/prefix-name-1.srx");
+    std::ifstream defaultGraphs[] = { std::ifstream( "data-r2/basic/data-6.ttl" ) };
+    DEFGRAPH_FILE_TEST("data-r2/basic/prefix-name-1.rq", "data-r2/basic/prefix-name-1.srx");
 }
 BOOST_AUTO_TEST_SUITE_END()
-
+#if 0
+BOOST_AUTO_TEST_CASE( Members_1 ) {
+    std::istringstream defaultGraphs[] ={ std::istringstream( "(<a> <b>) <p> (<x> <y> <z>) ." ) };
+    std::istringstream query(std::istringstream("SELECT*{members(?s)<p>members(?o)}"));
+    TestResultSet measured(defaultGraphs, sizeof(defaultGraphs)/sizeof(defaultGraphs[0]), NULL, query);
+    BOOST_CHECK_EQUAL(measured, ResultSet(&F, 
+					  "?s  ?o \n"
+					  "<a> <x>\n"
+					  "<a> <y>\n"
+					  "<a> <z>\n"
+					  "<b> <x>\n"
+					  "<b> <y>\n"
+					  "<b> <z>",
+					  true));
+}
+#endif
 // EOF
 
 /*
