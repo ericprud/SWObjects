7dc828475497559e0a0ba667d106cc8bb7dc3660
Eric Prudhommeaux <eric@w3.org>
2010-03-07 23:33:58 +0000

    + use of ResultSet

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -14,23 +14,39 @@ using namespace w3c_sw;
 
 int main () {
 
-    POSFactory F;
-    SPARQLfedDriver sparqlParser(BASE_URI, &F);
-    TurtleSDriver turtleParser(BASE_URI, &F);
+    POSFactory f;
+    RdfDB db;
+    SPARQLfedDriver sparqlParser(BASE_URI, &f);
+    TurtleSDriver turtleParser(BASE_URI, &f);
+    ResultSet rs(&f);
 
     IStreamContext q("select*{?s?p?o}", IStreamContext::STRING);
     if (sparqlParser.parse(q))
-	throw std::string("failed to parse query string \"") + q.nameStr + "\".";
-
-    RdfDB db;
+	throw std::string("failed to parse query string \"") + q.nameStr +"\".";
 
     turtleParser.setGraph(db.assureGraph(NULL));
-    IStreamContext ttl("<s1> <p1> <o1> .<s1> <p1> <o2> .<s1> <p1> <o1> .", IStreamContext::STRING);
+    IStreamContext ttl("<s1> <p1> <o1> ."
+		       "<s1> <p1> <o1> ." // identical to first statement.
+		       "<s1> <p1> <o2> .", IStreamContext::STRING);
     turtleParser.parse(ttl);
 
-    ResultSet rs(&F);
     sparqlParser.root->execute(&db, &rs);
-    std::cout << rs.size() << std::endl;
+    /* Print something about the result set. */
+    std::cout << "rs has " << rs.size() << " rows" << std::endl;
+    /* toString the result set. */
+    std::cout << rs;
+    /* Iterate across the result set. */
+    for (ResultSetConstIterator row = rs.begin();
+	 row != rs.end(); ++row) {
+	for (BindingSetIterator b = (*row)->begin(); b != (*row)->end(); ++b) {
+	    if (b != (*row)->begin())
+		std::cout << ", ";
+	    const POS* var = b->first;
+	    const POS* val = b->second.pos;
+	    std::cout << var->toString() << ":" << val->toString();
+	}
+	std::cout << std::endl;
+    }
     return 0;
 }
 
