b5415a0f2af8c5277452c2edb461d795f6555272
Eric Prudhommeaux <eric@w3.org>
2010-07-04 16:00:00 +0000

    + unittest

--- swig/python/t_SWObjects.py
+++ swig/python/t_SWObjects.py
@@ -1,52 +1,59 @@
 #!/usr/bin/python
-# file: runme.py
-# Test various properties of classes defined in separate modules
+# 
+# 
 
-print "Testing the %import directive"
+import unittest
 import SWObjects
 
-# Test access to constants.
-assert SWObjects.cvar.NS_xml == "http://www.w3.org/XML/1998/namespace"
+class TestSWObjects(unittest.TestCase):
 
-# Test a query.
-F = SWObjects.POSFactory()
-DB = SWObjects.RdfDB()
-default = DB.assureGraph(SWObjects.cvar.DefaultGraph)
-default.addTriplePattern(F.getTriple(
-        F.getURI(SWObjects.string("s", 1)), 
-        F.getURI(SWObjects.string("p1", 2)), 
-        F.getURI(SWObjects.string("o1", 2))
-        ))
-default.addTriplePattern(F.getTriple(
-        F.getURI(SWObjects.string("s", 1)), 
-        F.getURI(SWObjects.string("p2", 2)), 
-        F.getURI(SWObjects.string("o2", 2))
-        ))
-# print "DB: ", DB.toString().c_str()
-sparser = SWObjects.SPARQLfedDriver(SWObjects.string("", 1), F)
-query = SWObjects.SPARQLfedDriver.parseString("SELECT * { ?s <p1> ?o1 ; <p2> ?o2 }", F)
-# s = SWObjects.SPARQLSerializer()
-# query.express(s)
-# print "parsed: ", s.str().c_str()
-rs = SWObjects.ResultSet(F)
-query.execute(DB, rs)
-print rs.toString().c_str()
+    def setUp(self):
+        11
 
-bnodeMap = SWObjects.String2BNode()
-s = SWObjects.string("", 0)
-print type(F), ",", type(s), ",", type(bnodeMap)
-reference = SWObjects.ResultSet(F, SWObjects.string("""
-+------+------+-----+
+    def test_constants(self):
+        # Test access to constants.
+        self.assertEqual(SWObjects.cvar.NS_xml, "http://www.w3.org/XML/1998/namespace")
+
+    def test_s_p1_o1_p2_o2(self):
+        # Test a query.
+        F = SWObjects.POSFactory()
+        DB = SWObjects.RdfDB()
+        default = DB.assureGraph(SWObjects.cvar.DefaultGraph)
+        default.addTriplePattern(F.getTriple(
+                F.getURI(SWObjects.string("s", 1)), 
+                F.getURI(SWObjects.string("p1", 2)), 
+                F.getURI(SWObjects.string("o1", 2))
+                ))
+        default.addTriplePattern(F.getTriple(
+                F.getURI(SWObjects.string("s", 1)), 
+                F.getURI(SWObjects.string("p2", 2)), 
+                F.getURI(SWObjects.string("o2", 2))
+                ))
+        # print "DB: ", DB.toString().c_str()
+        sparser = SWObjects.SPARQLfedDriver(SWObjects.string("", 1), F)
+        query = SWObjects.SPARQLfedDriver.parseString("SELECT * { ?s <p1> ?o1 ; <p2> ?o2 }", F)
+        # s = SWObjects.SPARQLSerializer()
+        # query.express(s)
+        # print "parsed: ", s.str().c_str()
+        rs = SWObjects.ResultSet(F)
+        query.execute(DB, rs)
+        bnodeMap = SWObjects.String2BNode()
+        s = SWObjects.string("", 0)
+        print type(F), ",", type(s), ",", type(bnodeMap)
+        reference = SWObjects.ResultSet(F, SWObjects.string("""+------+------+-----+
 | ?o1  | ?o2  | ?s  |
 | <o1> | <o2> | <s> |
 +------+------+-----+
 """, 89), False);
-print "expected ", reference.toString().c_str()
-assert (reference == rs)
+        self.assertEqual(reference, rs)
 
-# reference = SWObjects.ResultSet(
+        # self.assertRaises(TypeError, random.shuffle, (1,2,3))
+        # self.assertTrue(element in self.seq)
 
-exit()
+if __name__ == '__main__':
+    unittest.main()
+
+# TODO:
 
 # turtleParser.setGraph(x)
 qstr = SWObjects.string("ASK {}", 1)
