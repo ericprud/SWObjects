852a28647635fe1de8b589887ca0964a454c10b7
Eric Prudhommeaux <eric@w3.org>
2010-12-05 04:41:24 +0000

    ~ moved tests to sparql2sparql/ ~ to_many_to_many works - ServiceOptimizer (moved to lib/ServiceOptimizer.hpp)

--- tests/test_QueryMap.cpp
+++ tests/test_QueryMap.cpp
@@ -444,7 +444,7 @@ struct SQLizerTest {
     }
 };
 
-BOOST_AUTO_TEST_SUITE( healthCare )
+BOOST_AUTO_TEST_SUITE( sparql2sparql )
     BOOST_AUTO_TEST_SUITE( simple )
 	BOOST_AUTO_TEST_CASE( order ) {
 	    RuleMapTest t("SELECT * { ?q1 <p2> ?q2 ; <p3> ?q3 }",
@@ -535,7 +535,7 @@ BOOST_AUTO_TEST_SUITE( healthCare )
 			  IStreamContext::STRING);
 	    BOOST_CHECK_EQUAL(*t.transformedNorm, *t.mapResultsNorm);
 	}
-#if 0
+
 	BOOST_AUTO_TEST_CASE( to_many_to_many ) {
 	    RuleMapTest t("PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
 			  "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
@@ -569,7 +569,12 @@ BOOST_AUTO_TEST_SUITE( healthCare )
 			  IStreamContext::STRING);
 	    BOOST_CHECK_EQUAL(*t.transformedNorm, *t.mapResultsNorm);
 	}
-#endif
+
+    BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE( healthCare )
+    BOOST_AUTO_TEST_SUITE( simple )
 	BOOST_AUTO_TEST_CASE( hl7_sdtm ) {
 	    RuleMapTest t("healthCare/simple/sdtm.rq", "healthCare/simple/hl7-sdtm.rq", "healthCare/simple/hl7.rq");
 	    BOOST_CHECK_EQUAL(*t.transformedNorm, *t.mapResultsNorm);
@@ -626,67 +631,3 @@ BOOST_AUTO_TEST_CASE( bsbm_1 ) {
 #endif
 
 
-    /** Example overload of SWObjectCanonicalizer to order SERVICEs by
-     *  some supplied order.
-     */
-
-    struct ServiceOptimizer : public SWObjectCanonicalizer {
-	std::map<const TTerm*, int> order;
-
-	struct ServiceOptimizerTableOperationSorter : public SWObjectCanonicalizer::TableOperationSorter {
-	    std::map<const TTerm*, int> order;
-
-	    struct OptimizingOperationSorter : public SWObjectCanonicalizer::TableOperationSorter::OperationSorter {
-		std::map<const TTerm*, int> order;
-
-		struct RHS_serviceGraphPattern_Opt : public SWObjectCanonicalizer::TableOperationSorter::OperationSorter::RHS_serviceGraphPattern {
-		    std::map<const TTerm*, int> order;
-		    RHS_serviceGraphPattern_Opt (AtomFactory* atomFactory, const TTerm* p_TTerm, const TableOperation* p_GroupGraphPattern, bool lex, std::map<const TTerm*, int> order)
-			: RHS_serviceGraphPattern(atomFactory, p_TTerm, p_GroupGraphPattern, lex), order(order)
-		    {  }
-		    virtual void serviceGraphPattern (const ServiceGraphPattern* const, const TTerm* p_TTerm, const TableOperation* p_GroupGraphPattern, AtomFactory*, bool) {
-			ret = l_TTerm != p_TTerm ? order[l_TTerm] < order[p_TTerm] ? SORT_lt : SORT_gt : _tableOperationOnOperation(p_GroupGraphPattern);
-		    }
-		};
-
-
-		OptimizingOperationSorter (AtomFactory* atomFactory, const TableOperation* rhs, std::map<const TTerm*, int> order)
-		    : OperationSorter(atomFactory, rhs), order(order)
-		{  }
-
-		virtual void serviceGraphPattern (const ServiceGraphPattern* const, const TTerm* p_TTerm, const TableOperation* p_GroupGraphPattern, AtomFactory* atomFactory, bool lexicalCompare) {
-		    RHS_serviceGraphPattern_Opt slave(atomFactory, p_TTerm, p_GroupGraphPattern, lexicalCompare, order);
-		    rhs->express(&slave);
-		    ret = slave.ret;
-		}
-		
-	    };
-	    ServiceOptimizerTableOperationSorter (AtomFactory* atomFactory, std::map<const TTerm*, int> order)
-		: TableOperationSorter (atomFactory), order(order)
-	    {  }
-	    bool operator () (const TableOperation* lhs, const TableOperation* rhs) {
-		OptimizingOperationSorter os(atomFactory, rhs, order);
-		lhs->express(&os);
-		return os.ret == SORT_lt;
-	    }
-	};
-
-	ServiceOptimizer (AtomFactory* atomFactory, std::map<const TTerm*, int> order)
-	    : SWObjectCanonicalizer (atomFactory), order(order)
-	{  }
-
-	/* _TableOperations factored out supporter function; virtual for MappedDuplicator. */
-	virtual void _TableOperations (const ProductionVector<const TableOperation*>* p_TableOperations, TableJunction* j) {
-	    ProductionVector<const TableOperation*> ops;
-	    for (std::vector<const TableOperation*>::const_iterator it = p_TableOperations->begin();
-	    	 it != p_TableOperations->end(); it++) {
-	    	(*it)->express(this);
-	    	ops.push_back(last.tableOperation);
-	    }
-	    std::sort(ops.begin(), ops.end(), TableOperationSorter(atomFactory));
-	    for (std::vector<const TableOperation*>::const_iterator it = ops.begin();
-	    	 it != ops.end(); it++)
-		j->addTableOperation(*it, false);
-	    ops.clear(); // don't delete members.
-	}
-    };
