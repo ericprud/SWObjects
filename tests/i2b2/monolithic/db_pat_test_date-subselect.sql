4b25b3cda61ea650222c894613a9c0cd588e3496
Eric Prudhommeaux <eric@w3.org>
2011-01-14 01:16:23 +0000

    ~ concientiously avoid same variable names in queries and rules

--- tests/i2b2/monolithic/db_pat_test_date-subselect.sql
+++ tests/i2b2/monolithic/db_pat_test_date-subselect.sql
@@ -1,6 +1,6 @@
-SELECT opt1.pat AS pat, opt1.t AS t, opt1.encDate AS encDate
+SELECT opt1.pat AS pat, opt1.typ AS typ, opt1.encDate AS encDate
        FROM (
-SELECT _all_0_entry.patient_num AS pat, _all_0_entry.start_date AS encDate, CONCAT("http://informatics.kumc.edu/404/i2b2demo/testtype#T", _all_0_concept.concept_cd) AS t, _all_0_concept.concept_cd AS _all_0_concept_cd
+SELECT _all_0_entry.patient_num AS pat, _all_0_entry.start_date AS encDate, CONCAT("http://informatics.kumc.edu/404/i2b2demo/testtype#T", _all_0_concept.concept_cd) AS typ, _all_0_concept.concept_cd AS _all_0_concept_cd
        FROM concept_dimension AS _all_0_concept
             INNER JOIN observation_fact AS _all_0_entry
  WHERE (_all_0_entry.concept_cd=_all_0_concept.concept_cd OR _all_0_entry.encounter_num="123")
