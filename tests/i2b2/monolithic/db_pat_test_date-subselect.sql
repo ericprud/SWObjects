85c4e423bf5b9eaa1c8166389482f87d7f20f5fc
Eric Prudhommeaux <eric@w3.org>
2012-01-06 13:17:30 +0000

    ~ fixed some inaccurate tests which failed to catch SQLizer's accidental mapping of conjunction expressions to disjuctions

--- tests/i2b2/monolithic/db_pat_test_date-subselect.sql
+++ tests/i2b2/monolithic/db_pat_test_date-subselect.sql
@@ -3,6 +3,6 @@ SELECT opt1.pat AS pat, opt1.typ AS typ, opt1.encDate AS encDate
 SELECT _all_0_entry.patient_num AS pat, _all_0_entry.start_date AS encDate, CONCAT("http://informatics.kumc.edu/404/i2b2demo/testtype#T", _all_0_concept.concept_cd) AS typ, _all_0_concept.concept_cd AS _all_0_concept_cd
        FROM concept_dimension AS _all_0_concept
             INNER JOIN observation_fact AS _all_0_entry
- WHERE (_all_0_entry.concept_cd=_all_0_concept.concept_cd OR _all_0_entry.encounter_num="123")
+ WHERE (_all_0_entry.concept_cd=_all_0_concept.concept_cd AND _all_0_entry.encounter_num="123")
              ) AS opt1
  LIMIT 10
