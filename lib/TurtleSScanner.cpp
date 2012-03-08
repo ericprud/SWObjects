8002b4f205f75573858e26e9f117a3aaf4e8126d
Eric Prudhommeaux <eric@w3.org>
2012-03-08 13:48:14 +0000

    ~ fix boolean value from lexer

--- lib/TurtleSScanner.cpp
+++ lib/TurtleSScanner.cpp
@@ -2060,12 +2060,12 @@ YY_RULE_SETUP
 case 16:
 YY_RULE_SETUP
 #line 129 "lib/TurtleSScanner.lpp"
-{yylval->p_BooleanRDFLiteral = driver->getBooleanRDFLiteral("true", yytext); return token::IT_true;}
+{yylval->p_BooleanRDFLiteral = driver->getBooleanRDFLiteral("true", true); return token::IT_true;}
 	YY_BREAK
 case 17:
 YY_RULE_SETUP
 #line 130 "lib/TurtleSScanner.lpp"
-{yylval->p_BooleanRDFLiteral = driver->getBooleanRDFLiteral("false", yytext); return token::IT_false;}
+{yylval->p_BooleanRDFLiteral = driver->getBooleanRDFLiteral("false", false); return token::IT_false;}
 	YY_BREAK
 case 18:
 YY_RULE_SETUP
