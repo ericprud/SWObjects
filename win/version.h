428dceafcdcc02c978f311a207fcbaeaea7d7743
ericP <eric@w3.org>
2014-09-13 02:04:24 -0400

    ~ track doc/version.h changes

--- win/version.h
+++ win/version.h
@@ -1,4 +1,4 @@
 #define GIT_URL "https://github.com/ericprud/SWObjects.git"
-#define GIT_Revision "2055"
+#define GIT_Revision "2061"
 #define GIT_Last_Changed_Author "ericprud <eric@w3.org>"
-#define GIT_Last_Changed_Date "2014-08-26 10:56:42 -0400"
+#define GIT_Last_Changed_Date "2014-09-11 03:18:35 -0400"
